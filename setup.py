249e724d7c44fb76702456d25861fdafeb398fac
Apaar Agarwal <apaaragarwal117@gmail.com>
2026-02-19 18:53:44 +0530

    Remove egg-info folders

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     version="0.0.1",
     author="Apaar",
     author_email='apaaragarwal117@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={"":"src"},
     install_requires=get_requirements('requirements.txt')
 )
